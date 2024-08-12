801a993c20882c72cc9a968efd9900b353d26f5f
Sri Harish Murugesu <78894390+sri123-afk@users.noreply.github.com>
2024-08-12 19:32:52 +0530

    reveal_commits_on_readme_and_seq

---
+++ new.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+# Generate the first 10 Fibonacci numbers
+print(fibonacci(10))
